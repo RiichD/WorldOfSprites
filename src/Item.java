185429e0e532b9064fe7299daa5cc5da9a721405
RiichD <47849926+RiichD@users.noreply.github.com>
2019-02-22 10:00:54 +0100

    Add files via upload

---
+++ src/Item.java
@@ -0,0 +1,15 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public interface Item{
+	public abstract void update();
+	public abstract Image getImage();
+}
---
+++ src/Water.java
@@ -0,0 +1,30 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Water implements Item{
+	private Image waterSprite;
+	
+	public Water() {
+		try {
+			waterSprite = ImageIO.read(new File("water.png"));
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	public Image getImage() {
+		return waterSprite;
+	}
+	
+	public void update() {
+	}
+}
