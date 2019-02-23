0ffba61395e3c5a463fe97451798b9c781231b36
RiichD <47849926+RiichD@users.noreply.github.com>
2019-02-23 13:31:58 +0100

    Add files via upload

---
+++ src/Tulipe.java
@@ -0,0 +1,33 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tulipe extends Flower {
+	private Image spriteTulipe;
+	
+	public Tulipe() {
+		super();
+		try {
+			spriteTulipe = ImageIO.read(new File("fleur3.png"));
+		} catch ( Exception e ) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	
+	public Image getImage() {
+		return spriteTulipe;
+	}
+	
+	public void update() {
+		
+	}
+	
+}
