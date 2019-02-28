8bf23efb9c6bb95e036e3bc9da5051964e30ce31
RiichD <47849926+RiichD@users.noreply.github.com>
2019-02-28 21:28:21 +0100

    smooth moving sprite

--- src/Human.java
+++ src/Human.java
@@ -12,8 +12,8 @@ import javax.swing.JPanel;
 @SuppressWarnings("unused")
 public class Human extends Agent{
 	private Image humanSprite;
-	private int drowning; //Se noie s'il reste 15 secondes dans l'eau
-	
+	private int drowning;
+	private int drowningtime=15;//Se noie s'il reste 15 secondes dans l'eau
 	public Human() {
 		super();
 		
@@ -48,7 +48,7 @@ public class Human extends Agent{
 	//Add
 	public void addDrowning() {
 		drowning++;
-		if (drowning == 15 ) {
+		if (drowning == drowningtime ) {
 			super.setAlive(false);
 		}
 	}
@@ -59,5 +59,12 @@ public class Human extends Agent{
 	}
 	
 	public void update() {
+	}
+	
+	public void draw(Graphics2D g, JFrame frame, int spriteLength) {
+		Graphics2D g2 = (Graphics2D) g;
+		
+		g2.drawImage(humanSprite, getPSpriteX(), getPSpriteY(), spriteLength, spriteLength, frame);
+
 	}
 }
