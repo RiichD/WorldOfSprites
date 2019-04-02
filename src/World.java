e84d3388fd1815b2ed783842af2f99fef51eccb0
RiichD <diep.richard75@gmail.com>
2019-04-02 22:27:54 +0200

    Perlin Noise

--- src/World.java
+++ src/World.java
@@ -52,13 +52,15 @@ public class World extends JPanel{
 	private int[][] fire; ///Si 0 rien, si >0 feu, sinon lave
 	
 	//Vitesse d'execution
-	private int delai = 1; //Delai pour la vitesse de deplacement d'agent
+	private int delai = 10; //Delai pour la vitesse de deplacement d'agent
 	private int delai2 = 0; //Delai pour la vitesse d'execution (d'affichage)
 	public static final int delai3 = 0; //Delai du main ( iteration )
 	private int lavaDelai = 200; //Delai permettant d'afficher la propagation de la lave progressivement
 	private int newCycleLSDelai = 5; //Delai lors du passage de la lave a la nouvelle terre
 	
 	//Attributs du monde
+	private int perlinSize = 30;
+	
 	private int nbHumanDepart = 25; //A chaque debut de cycle du monde, on ajoute un nombre d'agent au depart
 	private int nbChickenDepart = 25;
 	private int nbFoxDepart = 25;
@@ -145,48 +147,29 @@ public class World extends JPanel{
 			System.exit(-1);
 		}
 		
-		// Terrain predefini
-		for (i=0; i<x; i++) {
-			for (j=0; j<y; j++){
-				altitude[i][j] = 0;
-				fire[i][j] = 0;
+		//Terrain aleatoire
+		for (i=0; i<X; i++) {
+			for (j=0; j<Y; j++) {
+				terrain[i][j]=((int)((Get2DPerlinNoiseValue(i, j, perlinSize)+1)*2))%2;
 			}
 		}
 		
-		for (i=2; i<x-2; i++) {
-			if (Math.random() < 0.5 ) {
-				terrain[i][2] = sand;
-			} else {
-				terrain[i][2] = water;
-			} if (Math.random() < 0.5 ) {
-				terrain[x-3][i] = sand;
-			} else {
-				terrain[x-3][i] = water;
-			}
-			
-			terrain[i][3] = sand;
-			terrain[x-4][i] = sand;
-		}
-		
-		for (i=2; i<y-2; i++) {
-			if (Math.random() < 0.5 ) {
-				terrain[2][i] = sand;
-			}
-			if (Math.random() < 0.5 ) {
-				terrain[i][y-3] = sand;
-			}
-			
-			terrain[3][i] = sand;
-			terrain[i][y-4] = sand;
-		}
-		
-		for (i=4; i<x-4; i++) 
-			for (j=4; j<y-4; j++)
-				if (terrain[i][j]==water) {
-					terrain[i][j] = grass;
+		for (i=0; i<X; i++) {
+			for (j=0; j<Y; j++) {
+				if (terrain[i][j] == grass) {
+					boolean foundWater = false;
+					boolean foundGrass = false;
+					for (int n = -1; n <= 1 ; n++)
+						for (int m = -1; m <= 1 ; m++) {
+							if (i+n>=0 && i+n<X && j+m>=0 && j+m<Y) {
+								if (terrain[i+n][j+m] == water) foundWater = true;
+								if (terrain[i+n][j+m] == grass) foundGrass = true;
+							}
+						}
+					if (foundWater && foundGrass) terrain[i][j] = sand;
 				}
-		
-		// Fin du terrain predefini
+			}
+		}
 		
 		addInitiate();
 		
@@ -222,7 +205,7 @@ public class World extends JPanel{
 			addItem(new Rose());
 			addItem(new Tulip());
 			addItem(new Daisy());
-			addItem(new Tsunami());
+			//addItem(new Tsunami());
 		}
 		
 		for (int n=0;n<nbCactusDepart;n++)
@@ -273,6 +256,88 @@ public class World extends JPanel{
 				agents.add(a);
 		else
 			System.out.println("Ajout d'agent impossible");
+	}
+	
+	public void forceAddAgent(Agent a) {
+		if (a instanceof Human || a instanceof Chicken || a instanceof Fox || a instanceof Viper) {
+			int tailleInit = agents.size();
+			while(agents.size() == tailleInit) {
+				if (!(terrain[a.getX()][a.getY()]==water)) agents.add(a);
+			}
+		} else System.out.println("Ajout forcee d'agent impossible");
+	}
+	
+	//Perlin noise. Source : http://sdz.tdct.org/sdz/bruit-de-perlin.html
+	private double Get2DPerlinNoiseValue(float x, float y, float res)
+	{
+	    double tempX,tempY;
+	    int x0,y0,ii,jj,gi0,gi1,gi2,gi3;
+	    double unit = 1.0f/Math.sqrt(2);
+	    double tmp,s,t,u,v,Cx,Cy,Li1,Li2;
+	    double gradient2[][] = {{unit,unit},{-unit,unit},{unit,-unit},{-unit,-unit},{1,0},{-1,0},{0,1},{0,-1}};
+
+	    int perm[] =
+	       {151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,
+	        142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,
+	        203,117,35,11,32,57,177,33,88,237,149,56,87,174,20,125,136,171,168,68,175,
+	        74,165,71,134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,133,230,220,
+	        105,92,41,55,46,245,40,244,102,143,54,65,25,63,161,1,216,80,73,209,76,132,
+	        187,208,89,18,169,200,196,135,130,116,188,159,86,164,100,109,198,173,186,3,
+	        64,52,217,226,250,124,123,5,202,38,147,118,126,255,82,85,212,207,206,59,227,
+	        47,16,58,17,182,189,28,42,223,183,170,213,119,248,152,2,44,154,163,70,221,
+	        153,101,155,167,43,172,9,129,22,39,253,19,98,108,110,79,113,224,232,178,185,
+	        112,104,218,246,97,228,251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,
+	        235,249,14,239,107,49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,
+	        127,4,150,254,138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,
+	        156,180};
+
+	    //Adapter pour la résolution
+	    x /= res;
+	    y /= res;
+
+	    //On récupère les positions de la grille associée à (x,y)
+	    x0 = (int)(x);
+	    y0 = (int)(y);
+
+	    //Masquage
+	    ii = x0 & 255;
+	    jj = y0 & 255;
+
+	    //Pour récupérer les vecteurs
+	    gi0 = perm[ii + perm[jj]] % 8;
+	    gi1 = perm[ii + 1 + perm[jj]] % 8;
+	    gi2 = perm[ii + perm[jj + 1]] % 8;
+	    gi3 = perm[ii + 1 + perm[jj + 1]] % 8;
+
+	    //on récupère les vecteurs et on pondère
+	    tempX = x-x0;
+	    tempY = y-y0;
+	    s = gradient2[gi0][0]*tempX + gradient2[gi0][1]*tempY;
+
+	    tempX = x-(x0+1);
+	    tempY = y-y0;
+	    t = gradient2[gi1][0]*tempX + gradient2[gi1][1]*tempY;
+
+	    tempX = x-x0;
+	    tempY = y-(y0+1);
+	    u = gradient2[gi2][0]*tempX + gradient2[gi2][1]*tempY;
+
+	    tempX = x-(x0+1);
+	    tempY = y-(y0+1);
+	    v = gradient2[gi3][0]*tempX + gradient2[gi3][1]*tempY;
+
+
+	    //Lissage
+	    tmp = x-x0;
+	    Cx = 3 * tmp * tmp - 2 * tmp * tmp * tmp;
+
+	    Li1 = s + Cx*(t-s);
+	    Li2 = u + Cx*(v-u);
+
+	    tmp = y - y0;
+	    Cy = 3 * tmp * tmp - 2 * tmp * tmp * tmp;
+
+	    return Li1 + Cy*(Li2-Li1);
 	}
 	
 	//remove ou replace
@@ -339,7 +404,7 @@ public class World extends JPanel{
 					for ( int j = 0 ; j < copyFire.length ; j++ )
 						copyFire[i][j] = fire[i][j];
 				
-				//Boucle 
+				//Boucle permettant la propagation de la lave
 				for ( int i = 0 ; i < copyFire[0].length ; i++ )
 					for ( int j = 0 ; j < copyFire.length ; j++ ) {
 						if (fire[i][j]!=-1 && terrain[i][j]!=volcano) { //Si ce n'est pas de la lave, on cherche a le remplacer par de la lave s'il y a de la lave autour. Recherche sous forme de + (Voisinage de Von Neumann)
